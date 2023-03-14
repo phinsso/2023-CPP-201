b65721b845db7f7c995920e3ff5c49be9ccecefa
phinsso <s2225@e-mirim.hs.kr>
2023-03-14 09:54:23 +0900

    구구단 약간 효율적으로

--- 2023-CPP-201/반복문연습.cpp
+++ 2023-CPP-201/반복문연습.cpp
@@ -1,36 +1,19 @@
 #include<stdio.h>
 
 int main() {
-	
-	printf("2 X 1 = 2\n");
-	printf("2 X 2 = 4\n");
-	printf("2 X 3 = 6\n");
-	printf("2 X 4 = 8\n");
-	printf("2 X 5 = 10\n");
-	printf("2 X 6 = 12\n");
-	printf("2 X 7 = 14\n");
-	printf("2 X 8 = 16\n");
-	printf("2 X 9 = 18\n");
 
-	printf("3 X 1 = 3\n");
-	printf("3 X 2 = 6\n");
-	printf("3 X 3 = 9\n");
-	printf("3 X 4 = 12\n");
-	printf("3 X 5 = 15\n");
-	printf("3 X 6 = 18\n");
-	printf("3 X 7 = 21\n");
-	printf("3 X 8 = 24\n");
-	printf("3 X 9 = 27\n");
+	for (int i = 2; i < 10; i++) {
 
-	printf("4 X 1 = 4\n");
-	printf("4 X 2 = 8\n");
-	printf("4 X 3 = 12\n");
-	printf("4 X 4 = 16\n");
-	printf("4 X 5 = 20\n");
-	printf("4 X 6 = 24\n");
-	printf("4 X 7 = 28\n");
-	printf("4 X 8 = 32\n");
-	printf("4 X 9 = 36\n");
+		printf("%d X 1 = \n, i, (i * 1)");
+		printf("%d X 2 = \n, i, (i * 2)");
+		printf("%d X 3 = \n, i, (i * 3)");
+		printf("%d X 4 = \n, i, (i * 4)");
+		printf("%d X 5 = \n, i, (i * 5)");
+		printf("%d X 6 = \n, i, (i * 6)");
+		printf("%d X 7 = \n, i, (i * 7)");
+		printf("%d X 8 = \n, i, (i * 8)");
+		printf("%d X 9 = \n, i, (i * 9)");
+	}
 
 	return 0;
 }
