a19c2e8b5e5ac3e76b6b1f66aa58ab5d0c7c5497
phinsso <s2225@e-mirim.hs.kr>
2023-03-14 09:55:32 +0900

    구구단 약간 효율적으로

--- 2023-CPP-201/반복문연습.cpp
+++ 2023-CPP-201/반복문연습.cpp
@@ -4,15 +4,15 @@ int main() {
 
 	for (int i = 2; i < 10; i++) {
 
-		printf("%d X 1 = \n, i, (i * 1)");
-		printf("%d X 2 = \n, i, (i * 2)");
-		printf("%d X 3 = \n, i, (i * 3)");
-		printf("%d X 4 = \n, i, (i * 4)");
-		printf("%d X 5 = \n, i, (i * 5)");
-		printf("%d X 6 = \n, i, (i * 6)");
-		printf("%d X 7 = \n, i, (i * 7)");
-		printf("%d X 8 = \n, i, (i * 8)");
-		printf("%d X 9 = \n, i, (i * 9)");
+		printf("%d X 1 = %d\n", i, (i * 1));
+		printf("%d X 2 = %d\n", i, (i * 2));
+		printf("%d X 3 = %d\n", i, (i * 3));
+		printf("%d X 4 = %d\n", i, (i * 4));
+		printf("%d X 5 = %d\n", i, (i * 5));
+		printf("%d X 6 = %d\n", i, (i * 6));
+		printf("%d X 7 = %d\n", i, (i * 7));
+		printf("%d X 8 = %d\n", i, (i * 8));
+		printf("%d X 9 = %d\n", i, (i * 9));
 	}
 
 	return 0;
