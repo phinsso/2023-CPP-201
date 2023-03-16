493094514ce42c611c5f34f5ad1fcabbc51c90d6
phinsso <s2225@e-mirim.hs.kr>
2023-03-16 10:15:01 +0900

    namespace

--- 2023-CPP-201/grammar/CPP기본.cpp
+++ 2023-CPP-201/grammar/CPP기본.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 
 int main(void) {
+	
+	using namespace std; // 이름공간 (자주 사용하진 않음, std의 중복 사용을 피하기 위해서 사용)
+
 	// std::cout -> 출력을 위한 객체
 	// << -> 출력 연산자
 	// std::endl -> 개행(줄바꿈) 객체
-	std::cout << "Hello World" << std::endl; 
+	cout << "Hello World" << endl; 
 
 	return 0;
 }
