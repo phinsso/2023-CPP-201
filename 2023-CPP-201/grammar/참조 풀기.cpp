d977d07ddb0761b4ebc3c348f76d03aa722f759b
phinsso <s2225@e-mirim.hs.kr>
2023-03-21 09:37:04 +0900

    참조로 바꾸기

--- 2023-CPP-201/grammar/참조 풀기.cpp
+++ 2023-CPP-201/grammar/참조 풀기.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-void plus2(int* num) {
-	*num += 2;
+void plus2(int& num) {
+	num += 2;
 }
 
 int main(void) {
 
 	int a = 3;
-	plus2(&a);
+	plus2(a);
 
 	cout << a << endl;
 
