728e7d9dcc8d2f123b0972bc426c9b57f56ebef2
phinsso <s2225@e-mirim.hs.kr>
2023-03-21 09:05:46 +0900

    참조(reference)

--- 2023-CPP-201/grammar/참조형 변수.cpp
+++ 2023-CPP-201/grammar/참조형 변수.cpp
@@ -6,8 +6,8 @@ int main(void) {
 	
 	int a = 30;    //a: 30
 
-	int* pa = &a;  //pa: a의 주소값
-	*pa = 100;     //pa가 기리키는 대상(a)의 값을 바꾸겠다는 뜻
+	int& ra = a;  //int* pa = &a;
+	ra = 100;     //*pa = 100
 
 	cout << "a의 값은 " << a << endl;
 
