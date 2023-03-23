d0fdfb68765750cf4234002474861b3533d2b637
phinsso <s2225@e-mirim.hs.kr>
2023-03-23 10:02:52 +0900

    C스타일 문자열 복사 (시험에 안 나옴!)

---
+++ 2023-CPP-201/grammar/문자열복사.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+
+	char str[30] = { 0 }; //문자는 29개 들어감 (null값 1개)
+	char str2[30] = "c_insert";
+
+	for (int i = 0; str2[i] != '\0'; i++) {
+		str[i] = str2[i];
+	}
+
+	printf("%s \n",str);
+
+	return 0;
+}
