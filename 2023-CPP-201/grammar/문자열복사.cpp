fa3436b35a222746158e91127e714b1e2b4a42d3
phinsso <s2225@e-mirim.hs.kr>
2023-03-23 10:09:24 +0900

    C스타일 strcpy()

--- 2023-CPP-201/grammar/문자열복사.cpp
+++ 2023-CPP-201/grammar/문자열복사.cpp
@@ -6,11 +6,10 @@ int main(void) {
 	char str[30] = { 0 }; //문자는 29개 들어감 (null값 1개)
 	char str2[30] = "c_insert";
 
-	for (int i = 0; str2[i] != '\0'; i++) {
-		str[i] = str2[i];
-	}
+	strcpy(str, str2); //str에다가 str2에 있는 문자열을 복사하여 넣겠다는 뜻
 
 	printf("%s \n",str);
+	printf("%s \n", str2);
 
 	return 0;
 }
