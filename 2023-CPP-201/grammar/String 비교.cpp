665fc15786f04cc80a80e722943fd22c856142e2
phinsso <s2225@e-mirim.hs.kr>
2023-03-28 09:11:35 +0900

    C스타일 strcmp 문자열 비교

--- 2023-CPP-201/grammar/String 비교.cpp
+++ 2023-CPP-201/grammar/String 비교.cpp
@@ -13,33 +13,16 @@ int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	const int s1_l = strlen(str1);
-	const int s2_l = strlen(str2);
 
-	// s1_1과 s2_l 중 작은 값을 선택
-	const int l = s1_l < s2_l ? s1_l : s2_l;
-	
-	int i;
+	// 함수 인자 위치와 결과값(-1, 1) 주의해서 보기!
 
-	for (i = 0; i < l; i++) {
-		if (str1[i] < str2[i]) {
-			printf("str2 > str1\n");
-			break;
-		}
-		else if (str1[i] > str2[i]) {
-			printf("str1 > str2\n");
-			break;
-		}
-	}
-
-	if (i == l) {
-		if (s1_l > s2_l)
-			printf("str2 > str1\n");
-		else if (s1_l > s2_l)
-			printf("str2 < str1\n");
-		else
-			printf("str2 == str1");
-	}
+	int result = strcmp(str1, str2);
+	if (result == -1)
+		printf("str2 > str1");
+	else if (result == 1)
+		printf("str2 < str1");
+	else if (result == 0)
+		printf("str2 == str1");
 
 	return 0;
 }
