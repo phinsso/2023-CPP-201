ecd23fba01196cef99b830270da6f1ad5f50ee47
phinsso <s2225@e-mirim.hs.kr>
2023-03-30 10:46:31 +0900

    구조체

--- 2023-CPP-201/grammar/클래스기본.cpp
+++ 2023-CPP-201/grammar/클래스기본.cpp
@@ -5,20 +5,29 @@
 using namespace std;
 
 // 여러 데이터들을 하나의 의미있는 구조체로 묶어서 관리
-struct drink {
-	string moisture; //subun
-	string additive; //chumga
+// 구조체, 클래스는 단어의 첫 글자를 대문자로 한다
+struct Student {
+
+	string name;
+	// 문자열로 하지 않는 이유: 성능 (일반적으로 문자열은 정수 데이터보다 많은 메모리 공간을 요구하며, 정수는 비교연산이 한 번에 가능하나 문자열은 글자수만큼 반복하여 비교하기 때문
+	int stuId;
+	int age;
+	// 0: 남자, 1: 여자
+	int sex; // 유지보수를 위해 열거형(enum) 형으로 하는 것을 추천
+	string department;
+
 };
 
 int main(void) {
 
-	// (struct drink)가 자료형 (c++에서는 drink만 써도 되지만 헷갈리지 않도록)
-	struct drink strawberryMilk;
-	strawberryMilk.moisture = "서울우유";
-	strawberryMilk.additive = "킹스베리";
+	struct Student stu1;
+	stu1.name = "나소정";
+	stu1.stuId = 2106;
+	stu1.age = 18;
+	stu1.sex = 1;
+	stu1.department = "뉴미디어소프트웨어과";
 
-	cout << "수분: " << strawberryMilk.moisture << " 첨가물: " << strawberryMilk.additive << endl;
-	
+	cout << stu1.name << " " << stu1.stuId << " " << stu1.age << " " << stu1.sex << " " << stu1.major << endl;
 
 	return 0;
 }
