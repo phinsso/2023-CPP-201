ce6d47b7eef256f109fe17e09027f43aa6957154
phinsso <s2225@e-mirim.hs.kr>
2023-03-30 10:02:56 +0900

    구조체 기본

---
+++ 2023-CPP-201/grammar/string 과제.cpp
@@ -0,0 +1,18 @@
+﻿//C++
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void) {
+
+	string str1 = "Hello";
+	string str2 = "Hello";
+
+	if (str2 == str1)
+		cout << str1 << " 같다 " << str2;
+	else
+		cout << str1 << " 다르다 " << str2;
+
+	return 0;
+}
---
+++ 2023-CPP-201/grammar/클래스기본.cpp
@@ -0,0 +1,24 @@
+﻿//C++
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+// 여러 데이터들을 하나의 의미있는 구조체로 묶어서 관리
+struct drink {
+	string moisture; //subun
+	string additive; //chumga
+};
+
+int main(void) {
+
+	// (struct drink)가 자료형 (c++에서는 drink만 써도 되지만 헷갈리지 않도록)
+	struct drink strawberryMilk;
+	strawberryMilk.moisture = "서울우유";
+	strawberryMilk.additive = "킹스베리";
+
+	cout << "수분: " << strawberryMilk.moisture << " 첨가물: " << strawberryMilk.additive << endl;
+	
+
+	return 0;
+}
