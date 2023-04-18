8cadc33e6e2ffb849b9d417cb4ac47455080c574
phinsso <s2225@e-mirim.hs.kr>
2023-04-18 10:05:44 +0900

    문자열 동적할당

--- 2023-CPP-201/grammar/클래스 중급.cpp
+++ 2023-CPP-201/grammar/클래스 중급.cpp
@@ -1,4 +1,6 @@
 ﻿#include<iostream>
+#include<string>
+#include<string.h>
 
 using namespace std;
 
@@ -6,15 +8,18 @@ class MString {
 public:
 	//생성자
 	MString(const char* str) { // 포인터로 사용하려면 const 붙여야 함
-		c_str_ = str;
-	}
+		// 필요한 길이만큼 문자열 동적할당
+		unsigned int str_length = strlen(str);
+		c_str_ = new char[str_length + 1]; // null 문자 공간 만들어줌 => + 1
+		strcpy(c_str_, str);
+	} 
 
 	unsigned int size() { return size_; } //getter
-	const char* c_str() { return c_str_; } //getter
+	char* c_str() { return c_str_; } //getter
 
 private:
 	unsigned int size_; // 문자열의 길이
-	const char* c_str_;		// 문자열을 가리키는 주소
+	char* c_str_;		// 문자열을 가리키는 주소
 };
 
 int main(void) {
