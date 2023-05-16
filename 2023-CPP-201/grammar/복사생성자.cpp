4a187cbf0b9e5ae0c172474149fb5eeb273cfb72
phinsso <s2225@e-mirim.hs.kr>
2023-05-16 09:05:51 +0900

    [C++] 소멸자

--- 2023-CPP-201/grammar/복사생성자.cpp
+++ 2023-CPP-201/grammar/복사생성자.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 class Character {
 public:
-	// 일반 생성자
+	// 일반 생성자(깊은 복사)
 	Character(const char* str)
 	{
 		str_ = new char[strlen(str) + 1]; // 매개변수로 넘겨받는 str의 값 + 1 (널문자 들어갈 공간)만큼 배열을 잡는다
 		strcpy(str_, str); // 매개변수 str의 내용을 멤버변수 str_에 복사
 		cout << "일반생성자 호출" << endl;
+	}
+
+	// 소멸자
+	~Character() {
+		delete[] str_; // str_ 배열을 동적할당 해제
+		cout << "소멸자 호출" << endl;
 	}
 
 private:
