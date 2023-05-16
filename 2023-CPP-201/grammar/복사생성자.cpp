49a5ff3a81b30e924f8b3bc76acf8e07f70226b5
phinsso <s2225@e-mirim.hs.kr>
2023-05-16 09:01:51 +0900

    [C++] 일반생성자(깊은 복사)

---
+++ 2023-CPP-201/grammar/복사생성자.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class Character {
+public:
+	// 일반 생성자
+	Character(const char* str)
+	{
+		str_ = new char[strlen(str) + 1]; // 매개변수로 넘겨받는 str의 값 + 1 (널문자 들어갈 공간)만큼 배열을 잡는다
+		strcpy(str_, str); // 매개변수 str의 내용을 멤버변수 str_에 복사
+		cout << "일반생성자 호출" << endl;
+	}
+
+private:
+	char* str_;
+
+};
+
+int main(void) {
+
+	Character c1 = Character("abc");
+
+	return 0;
+}
