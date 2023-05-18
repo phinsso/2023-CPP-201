61eecbed4a56dacd9d4da119777d73c3852ae702
phinsso <s2225@e-mirim.hs.kr>
2023-05-18 11:05:37 +0900

    [c++] keyboard::Up/Down

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -6,6 +6,11 @@ int main(void) {
 
 	RenderWindow window(VideoMode(640, 480), "Snake Game");
 
+	RectangleShape snake;
+	snake.setPosition(200, 300);
+	snake.setSize(Vector2f(30, 30));
+	snake.setFillColor(Color::Green);
+
 	while (window.isOpen())
 	{
 		Event e;
@@ -16,7 +21,15 @@ int main(void) {
 				window.close();
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+			snake.move(0, -1);
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+			snake.move(0, 1);
+
 		window.clear();
+
+		window.draw(snake);
+
 		window.display();
 
 	}
