76c4d956569081973bcb57290bdfc048402d7b52
phinsso <s2225@e-mirim.hs.kr>
2023-05-18 10:44:01 +0900

    [c++] Event::Colosed

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -6,5 +6,20 @@ int main(void) {
 
 	RenderWindow window(VideoMode(640, 480), "Snake Game");
 
+	while (window.isOpen())
+	{
+		Event e;
+		while (window.pollEvent(e))
+		{
+			// 창 닫기를 눌렀을 때 (윈도우 창의 X 표시) 창이 닫아지도록
+			if (e.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+
+	}
+
 	return 0;
 }
