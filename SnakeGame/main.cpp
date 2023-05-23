ecf148243d3aac02384457fea96d1dc6fbbad8ff
phinsso <s2225@e-mirim.hs.kr>
2023-05-23 10:15:12 +0900

    [C++] [SFML] 좌표체계를 그리드화

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -9,22 +9,30 @@ int main(void) {
 	const int WIDTH = 1000;
 	const int HEIGHT = 800;
 
-	// 한 칸을 40으로
-	int block = 40;
+	int block = 40; // 한 칸을 40으로
+	const int w = WIDTH / block;
+	const int h = HEIGHT / block;
+
+
+	
 
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Snake Game");
-	window.setFramerateLimit(60); // snake 움직이는 속도 조절 (1초에 60번의 작업이 이루어지도록 프레임을 조절) -> 사양에 관계없이 똑같은 속도로 처리됨 
+	window.setFramerateLimit(30); // snake 움직이는 속도 조절 (1초에 30번의 작업이 이루어지도록 프레임을 조절) -> 사양에 관계없이 똑같은 속도로 처리됨 
 
 	srand(time(NULL));
 
 
 	RectangleShape snake;
-	snake.setPosition(200, 300);
+	int snake_x = 3;
+	int snake_y = 3;
+	snake.setPosition(snake_x * block , snake_y * block);
 	snake.setSize(Vector2f(block, block));
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(rand()%WIDTH-block, rand()%HEIGHT-block);
+	int apple_x = rand() % 10;
+	int apple_y = rand() % 10;
+	apple.setPosition(apple_x * block, apple_y * block);
 	apple.setSize(Vector2f(block, block));
 	apple.setFillColor(Color::Red);
 
@@ -40,20 +48,34 @@ int main(void) {
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Up))
-			snake.move(0, -5);
+		{
+			snake_y--;
+			snake.move(0, -block);
+		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down))
-			snake.move(0, 5);
+		{
+			snake_y++;
+			snake.move(0, block);
+		}
 		else if (Keyboard::isKeyPressed(Keyboard::Left))
-			snake.move(-5, 0);
+		{
+			snake_x--;
+			snake.move(-block, 0);
+		}
 		else if (Keyboard::isKeyPressed(Keyboard::Right))
-			snake.move(5,0);
+		{
+			snake_x++;
+			snake.move(block, 0);
+		}
 
 		// update
 
 		// 뱀이 사과를 먹었을 때
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
 		{
-			apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
+			apple_x = rand() % w;
+			apple_y = rand() % h;
+			apple.setPosition(apple_x * block, apple_y * block);
 		}
 
 
