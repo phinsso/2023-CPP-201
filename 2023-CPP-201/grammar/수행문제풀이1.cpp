48251328e05ae3f7b82b0f1817da6a55b3d2719b
phinsso <s2225@e-mirim.hs.kr>
2023-05-25 09:58:18 +0900

    [C++] 수행평가 문제 풀이 1 - 얕은 복사

---
+++ 2023-CPP-201/grammar/수행문제풀이1.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class IntArray {
+
+public:
+	IntArray(int* arr, int size)
+	{
+		arr_ = new int[size];
+		size_ = size;
+
+		for (int i = 0; i < size; i++) {
+			arr_[i] = arr[i];
+		}
+	}
+
+	IntArray(const IntArray& rhs)
+		: size_(rhs.size_), arr_(rhs.arr_)
+	{
+		// size_ = rhs.size_;
+		// arr_ = rhs.arr_;
+	}
+
+private:
+	int* arr_;
+	int size_;
+};
+
+int main(void) {
+	int arr[] = { 1, 2, 3 };
+	IntArray ia = IntArray(arr, sizeof(arr)/sizeof(*arr));
+	IntArray ca = ia;
+
+	return 0;
+}
