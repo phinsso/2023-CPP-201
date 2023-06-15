f42f7cecfa1201a1508236d34e8644485f127017
phinsso <s2225@e-mirim.hs.kr>
2023-06-15 09:59:54 +0900

    [C++] 다형성 개념

--- 2023-CPP-201/grammar/상속.cpp
+++ 2023-CPP-201/grammar/상속.cpp
@@ -10,23 +10,11 @@ public:
 	Animal(int color, int age) : color_(color), age_(age)
 	{
 		cout << "Animal 생성자()" << endl;
-		cout << age_ << "살" << endl;
-
-		switch (color_) {
-		case COLOR_RED:
-			cout << "빨간색" << endl; break;
-		case COLOR_GREEN:
-			cout << "초록색" << endl; break;
-		case COLOR_BLUE:
-			cout << "파란색" << endl; break;
-		default:
-			cout << "유효하지 않은 색" << endl; break;
-		}
 	}
 
 	~Animal() { cout << "Animal 소멸자()" << endl; }
 
-	void Bite(void) { cout << "Animal 물다" << endl; }
+	void Roar(void) { cout << "Animal 짖다" << endl; }
 	void Eat(void) { cout << "Animal 먹다" << endl; }
 	void Sleep(void) { cout << "Animal 자다" << endl; }
 
@@ -41,10 +29,16 @@ public:
 	Rabbit(int color, int age, int ear_length) : Animal(color, age), ear_length_(ear_length)
 	{
 		cout << "Rabbit 생성자()" << endl;
-		cout << "귀 길이 " << ear_length_ << endl;
 	}
 
 	~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
+
+	// 함수 override
+	// 다형성 (polymorphism) : 시그니처가 같은 함수임에도 다르게 실행되는 것
+	// (시그니처: 이름, 반환형, 매개변수가 모두 같은 것)
+	void Roar(void) {
+		cout << "깡총깡총" << endl;
+	}
 
 private:
 	int ear_length_;
@@ -53,7 +47,7 @@ private:
 int main(void)
 {
 	Rabbit* rabbit = new Rabbit(COLOR_RED, 3, 20); // 부모의 생성자 호출
-	rabbit->Bite(); // 부모의 멤버함수 호출
+	rabbit->Roar(); // 부모의 멤버함수 호출
 	delete rabbit;
 	
 }
---
+++ 2023-CPP-201/grammar/상속_문제풀기.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+
+class Person {
+
+public:
+	Person(string name, int hakbun) : name_(name), hakbun_(hakbun)
+	{
+	}
+
+	void Printshow() {
+		cout << "이름: " << name_ << "  학번: " << hakbun_ << endl;
+	}
+
+	~Person() { cout << "Person 소멸자()" << endl; }
+
+private:
+	string name_;
+	int hakbun_;
+};
+
+class Student : public Person {
+
+public: 
+	Student(string name, int hakbun, string university) : Person(name, hakbun), university_(university)
+	{
+		cout << "대학교: " << university_ << endl;
+	}
+
+	~Student() { cout << "Student 소멸자()" << endl; }
+
+private:
+	string university_;
+
+};
+
+int main(void) {
+	Student* stu = new Student("김미림", 2106, "미림대학교");
+	stu->Printshow();
+	delete stu;
+}
