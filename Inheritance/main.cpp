bd4ac97f98dcd54426097fb9a72c6b8d06cc1700
phinsso <naso01247@gmail.com>
2023-08-01 08:54:21 +0900

    [상속] 상속을 위한 새로운 프로젝트 생성

---
+++ Inheritance/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(void)
+{
+	RenderWindow window(VideoMode(1000, 800), "Sangsok");
+
+	while (window.isOpen()) {
+		Event e;
+
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}
