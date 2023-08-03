df5a0047c38c57675d50aaac8756b24cbb48bb64
phinsso <naso01247@gmail.com>
2023-08-03 09:48:58 +0900

    [상속] 적 하나 만들기

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,4 +1,6 @@
 ﻿#include <SFML/Graphics.hpp>
+#include<stdlib.h>
+#include<time.h>
 
 using namespace sf;
 
@@ -37,6 +39,8 @@ private:
 
 int main(void)
 {
+	srand((unsigned int)time(NULL));
+
 	RenderWindow window(VideoMode(1000, 800), "Sangsok");
 	window.setFramerateLimit(60);
 
@@ -45,7 +49,13 @@ int main(void)
 	sp1.setSize(Vector2f(50, 50));
 	sp1.setFillColor(Color::Blue);
 
+	RectangleShape se1;
+	se1.setPosition(rand() % 800, rand() % 600);
+	se1.setSize(Vector2f(40, 40));
+	se1.setFillColor(Color::Yellow);
+
 	Entity* player = new Entity(3, 5, &sp1);
+	Entity* enemy1 = new Entity(1, 5, &se1);
 
 	while (window.isOpen()) {
 		Event e;
@@ -77,6 +87,8 @@ int main(void)
 		// Render
 
 		window.clear();
+
+		window.draw(enemy1->get_sprite());
 		window.draw(player->get_sprite());
 		window.display();
 	}
