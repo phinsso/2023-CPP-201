964fb5a6f8e7f27d9df646a2b9e8cdf7c22b8ac6
phinsso <naso01247@gmail.com>
2023-08-29 09:10:25 +0900

    [협업] 파일 분할 전 단계

---
+++ 2023-CPP-201/grammar/템플릿테스트.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Hero {
+public:
+	// 생성자
+	Hero(string name, T health, T attack, T defence)
+		: name(name), health(health), attack(attack), defence(defence) {}
+
+	void Print(void) {
+		cout << "이름: " << name << endl;
+		cout << "체력: " << health << endl;
+		cout << "공격: " << attack << endl;
+		cout << "방어: " << defence << endl;
+	}
+
+private:
+	string name;
+	T health;
+	T attack;
+	T defence;
+};
+
+int main(void) {
+
+	Hero<int> *h1 = new Hero<int>("소정", 1000, 80, 150);
+	h1->Print();
+	delete h1;
+
+	return 0;
+}
---
+++ FileSplit/main.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	void sing(void) {}
+
+private:
+	int hp_;
+};
+
+class Shark : public Animal {
+public:
+	void sing(void) { cout << "아기상어 뚜루루 뚜루" << endl; }
+
+private:
+	int teeth_;
+};
+
+class Whale : public Animal {
+public:
+	void sing(void) { cout << "우 우우 우 우" << endl; }
+
+private:
+	int bread_;
+
+};
+
+int main(void) {
+	Animal a;
+	Shark jaws;
+	Whale lky;
+
+	a.sing();
+	jaws.sing();
+	lky.sing();
+}
