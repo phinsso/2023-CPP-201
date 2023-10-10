d24936e0c578744cb840731d113d1949a3741c4e
phinsso <naso01247@gmail.com>
2023-10-10 09:51:15 +0900

    [C++] map

---
+++ 2023-CPP-201/grammar/맵.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	// key: 문자열, value: 정수
+	map<string, int> moving;
+
+	moving["번개맨"] = 1987;
+	moving["장희수"] = 2000;
+	moving["봉평"] = 1959;
+
+	map<string, int>::iterator iter;
+
+	// map은 순서가 없기 때문에 iterator로만 반복을 해야한다.
+	for (iter = moving.begin(); iter != moving.end(); iter++) {
+		// first: key, second: value
+		cout << iter->first << "   " <<  iter->second << endl;
+	}
+
+}
