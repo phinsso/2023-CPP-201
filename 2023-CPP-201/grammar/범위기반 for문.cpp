713835bc8502837d00c8762b2bc6f42ef134d364
phinsso <naso01247@gmail.com>
2023-10-17 09:59:28 +0900

    auto : 자료형 자동 설정

--- 2023-CPP-201/grammar/범위기반 for문.cpp
+++ 2023-CPP-201/grammar/범위기반 for문.cpp
@@ -6,7 +6,11 @@ int main() {
 
 	double arr[] = {1.1, 2.2, 3.3, 4.4};
 
-	for (double x : arr) {
+	// a의 자료형은 너무 명확하게 int란 걸 알 수 있다.
+	auto a = 4;
+
+	// x의 자료형은 너무 명확하게 double이란 걸 알 수 있다.
+	for (auto x : arr) {
 		cout << x << endl;
 	}
 
