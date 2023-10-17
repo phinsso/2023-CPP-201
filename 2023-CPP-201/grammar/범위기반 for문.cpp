650ec575305c0a920de3cef7720933bc6968fd07
phinsso <naso01247@gmail.com>
2023-10-17 09:50:29 +0900

    전통적인 배열 반복문

---
+++ 2023-CPP-201/grammar/범위기반 for문.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double arr[] = {1.1, 2.2, 3.3, 4.4};
+
+	for (int i = 0; i < 4; i++) {
+		cout << arr[i] << endl;
+	}
+
+	return 0;
+}
